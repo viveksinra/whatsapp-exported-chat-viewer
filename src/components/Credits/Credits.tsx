5c364586395a843a8d2a500fb37858d64182326b
viveksinra <vivekkrvks@gmail.com>
2024-04-23 14:12:39 +0530

    first completion

---
+++ src/components/Credits/Credits.tsx
@@ -0,0 +1,16 @@
+function Credits() {
+  return (
+    <div>
+      <small>
+        Made by <a href="https://viveksinra.com">Vivek</a>
+        <br />
+        View{' '}
+        <a href="https://github.com/viveksinra/whatsapp-exported-chat-viewer">
+          Source code
+        </a>
+      </small>
+    </div>
+  );
+}
+
+export default Credits;
